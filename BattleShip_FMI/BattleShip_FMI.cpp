a276a1068f1e64f86b7e485e84cc22863b2f495f
Serhan Chavdarliev <67830778+darimachine@users.noreply.github.com>
2024-01-11 23:08:31 +0200

    Update BattleShip_FMI.cpp

--- BattleShip_FMI/BattleShip_FMI.cpp
+++ BattleShip_FMI/BattleShip_FMI.cpp
@@ -1,5 +1,4 @@
-﻿// BattleShip_FMI.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 #include <iostream>
 #include <vector>
 #include <iomanip> // for setW()
